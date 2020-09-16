392bf1715a775e7cdeca98d77cd0971b98aea63d
= <=>
2020-09-16 23:34:05 +0530

    Data Structures in C

--- LinearQueue/main.c
+++ LinearQueue/main.c
@@ -5,96 +5,89 @@
 int queue[MAX];
 int FRONT=-1, REAR =-1;
 
-void enqueue()
+void enqueue(int x)
 {
-
-    if(REAR == MAX-1)
+    if(REAR = MAX-1)
     {
-        printf("\nOverflow!!!");
+
+        printf("Overflow!!!");
+        return;
+    }
+    else if(FRONT == -1 && REAR ==-1)
+    {
+        FRONT++;
+        REAR++;
     }
     else
     {
-
-        if(FRONT==-1)
-        {
-            FRONT++;
-        }
         REAR++;
-        int temp;
-        printf("\nEnter the element to enter:");
-        scanf("%i",&temp);
-        queue[REAR] = temp;
-        printf("\nThe element was inserted successfully!!!");
     }
+    queue[REAR] = x;
+    return;
 }
 
 void dequeue()
 {
-    if(FRONT==-1 && REAR ==-1)
+    if(FRONT ==-1 || FRONT>REAR)
     {
-        printf("\nUnderflow!!!!");
-    }
-    else if(FRONT==REAR)
-    {
-        FRONT=-1;
-        REAR=-1;
-        printf("\nElement removed successfully!!!");
+        printf("\nUnderflow!!!");
     }
     else
     {
 
         FRONT++;
-        printf("\nElement removed successfully!!!");
     }
+    return;
 }
 
 void peek()
 {
-    if(FRONT== -1)
-        printf("\nThe Queue is empty!!!");
+    if(REAR==-1)
+    {
+        printf("\nUnderflow");
+    }
     else
     {
-        printf("\nPeek: %i", queue[FRONT]);
+        printf("%d",queue[REAR]);
     }
 }
 
 void display()
 {
-
-    if(FRONT==-1 && REAR ==-1)
+    if(REAR == -1 || FRONT>REAR)
     {
-
-        printf("\nThe Queue is empty!!!");
+        printf("\nUnderflow");
     }
     else
     {
         printf("\nThe Queue is as follows:\n");
-        for(int i=FRONT; i<=REAR;i++)
+        for(int i=FRONT;i<=REAR;i++)
         {
-
-            printf(" %i",queue[i]);
+            printf("%d ",queue[i]);
         }
     }
 }
 
 int main()
 {
-    int decision;
+    int choice;
+    int val;
     do
     {
-
-        printf("\n*********MAIN MENU********");
-        printf("\n 1.Insert an Element in the Queue");
-        printf("\n 2.Delete an Element in the Queue");
-        printf("\n 3.Peek first element");
-        printf("\n 4.Display the Queue");
-        printf("\n.5.Exit");
-        printf("\n Enter your choice:");
-        scanf("%i",&decision);
-        switch(decision)
+        printf("\n************MAIN MENU***************");
+        printf("\n1.Insert an Element");
+        printf("\n2.Delete an Element");
+        printf("\n3.Peek");
+        printf("\n4.Display");
+        printf("\n5.EXIT");
+        printf("\nEnter your choice:");
+        scanf("%d",&choice);
+        switch(choice)
         {
             case 1:
-                enqueue();
+                printf("\nEnter the element to be inserted:");
+                scanf("%d",&val);
+                enqueue(val);
                 break;
             case 2:
                 dequeue();
@@ -105,7 +98,8 @@ int main()
             case 4:
                 display();
                 break;
+
         }
-    }
-    while(decision!=5);
+
+    }while(choice!=5);
 }
